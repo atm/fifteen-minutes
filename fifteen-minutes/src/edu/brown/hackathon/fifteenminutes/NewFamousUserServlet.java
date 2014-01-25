8f35277706ee31aa213cf3a6f375e60c0814fac2
Aaron T. Myers <atm@cloudera.com>
2014-01-25 09:39:47 -0500

    Make the new random user not repeat the last one. Add some debug output.

--- fifteen-minutes/src/edu/brown/hackathon/fifteenminutes/NewFamousUserServlet.java
+++ fifteen-minutes/src/edu/brown/hackathon/fifteenminutes/NewFamousUserServlet.java
@@ -21,11 +21,10 @@ public class NewFamousUserServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     
     long oldFamousUser = IsFamousServlet.getUserIdOfFamousUser();
-    long newFamousUser = selectRandomUserId();
+    long newFamousUser = selectRandomUserId(oldFamousUser);
     
     // Make the Instagram API calls.
-    // TODO(atm): this won't work until we actually have access tokens and real user IDs in the datastore.
-    // makeFamous(newFamousUser, oldFamousUser);
+    makeFamous(newFamousUser, oldFamousUser);
     
     // Set the new famous user in the DB.
     Entity famousUser = new Entity("FamousUser");
@@ -40,19 +39,23 @@ public class NewFamousUserServlet extends HttpServlet {
     resp.getWriter().println(new Gson().toJson(ur));
   }
   
-  private static long selectRandomUserId() {
+  private static long selectRandomUserId(long oldFamousUser) {
     Query query = new Query("User");
+    query.setFilter(new Query.FilterPredicate("user_id", FilterOperator.NOT_EQUAL, oldFamousUser));
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1000));
     Entity result = results.get(new Random().nextInt(results.size()));
     return UserResource.parseUserIdFromEntity(result);
   }
   
+  // TODO(atm): this won't work until we actually have access tokens and real user IDs in the datastore.
   private static void makeFamous(long newFamousUser, long oldFamousUser) throws IOException {
     Query query = new Query("User");
     query.setFilter(new Query.FilterPredicate("user_id", FilterOperator.NOT_EQUAL, newFamousUser));
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1000));
+    System.out.println("====> going to make " + oldFamousUser + " unfamous");
+    System.out.println("====> going to make " + newFamousUser + " hella famous");
     for (Entity result : results) {
       String accessToken = (String) result.getProperty("access_token");
       String unfollowRequestString = "http://api.instagram.com/v1/users/" + oldFamousUser
@@ -61,9 +64,10 @@ public class NewFamousUserServlet extends HttpServlet {
       String followRequestString = "http://api.instagram.com/v1/users/" + newFamousUser
           + "/relationship?access_token=" + accessToken
           + "&ACTION=follow";
-      
-      HttpUtil.doHttpRequest(unfollowRequestString);
-      HttpUtil.doHttpRequest(followRequestString);
+      System.out.println("====> calling: " + unfollowRequestString);
+      System.out.println("====> calling: " + followRequestString);
+      //HttpUtil.doHttpRequest(unfollowRequestString);
+      //HttpUtil.doHttpRequest(followRequestString);
     }
   }
 }
