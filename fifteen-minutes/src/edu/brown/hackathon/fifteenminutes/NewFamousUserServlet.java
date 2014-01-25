ec282a55fbc0c4935fa47acf391b3a2fe11a9847
Aaron T. Myers <atm@cloudera.com>
2014-01-25 01:38:52 -0500

    Remove some unused imports.

--- fifteen-minutes/src/edu/brown/hackathon/fifteenminutes/NewFamousUserServlet.java
+++ fifteen-minutes/src/edu/brown/hackathon/fifteenminutes/NewFamousUserServlet.java
@@ -1,20 +1,18 @@
 package edu.brown.hackathon.fifteenminutes;
 
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
