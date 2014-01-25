e300d73022926ce328c3bd73783fd92ea6fa2fd4
Aaron T. Myers <atm@cloudera.com>
2014-01-25 01:37:22 -0500

    Redo random user selection. Won't scale past 1k users.

--- fifteen-minutes/src/edu/brown/hackathon/fifteenminutes/NewFamousUserServlet.java
+++ fifteen-minutes/src/edu/brown/hackathon/fifteenminutes/NewFamousUserServlet.java
@@ -35,17 +35,10 @@ public class NewFamousUserServlet extends HttpServlet {
   }
   
   private static long selectRandomUserId() {
-    List<Entity> results = new LinkedList<Entity>();
-    Random rand = new Random();
-    while (results.size() == 0) {
-      Filter randomFilter = new FilterPredicate("rand_number",
-          FilterOperator.GREATER_THAN_OR_EQUAL, rand.nextFloat());
-      Query query = new Query("User");
-      query.addSort("rand_number");
-      query.setFilter(randomFilter);
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1));
-    }
-    return UserResource.parseUserIdFromEntity(results.get(0));
+    Query query = new Query("User");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1000));
+    Entity result = results.get(new Random().nextInt(results.size()));
+    return UserResource.parseUserIdFromEntity(result);
   }
 }
